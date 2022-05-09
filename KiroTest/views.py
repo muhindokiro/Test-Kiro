cc8a729d0772a878d72862f6d5a4ccc1e443fe12
Muhindo Kiro <blaize1143@gmail.com>
2022-05-09 18:11:16 +0300

    write function in views to serialize all objects from drink model

--- KiroTest/views.py
+++ KiroTest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Drink
+from django.http import JsonResponse
+from .serializers import DrinkSerializer
+
 
 # Create your views here.
+def cocktail_list(request):
+    
+    cocktails =  Drink.objects.all()
+    
+    serializer = DrinkSerializer(cocktails, many=True)
+    return JsonResponse(serializer.data)

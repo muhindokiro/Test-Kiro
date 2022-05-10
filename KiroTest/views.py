92030008698b3788243c39b72ad1085147418d17
Muhindo Kiro <blaize1143@gmail.com>
2022-05-10 12:57:59 +0300

    import apiview decorator,,add get and post method

--- KiroTest/views.py
+++ KiroTest/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Drink
 from django.http import JsonResponse
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
-def cocktail_list(request):
+@api_view('GET', 'POST')
+def cocktail_list(request): 
 
     cocktails =  Drink.objects.all()
 
     serializer = DrinkSerializer(cocktails, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return JsonResponse({'cocktails': serializer.data})

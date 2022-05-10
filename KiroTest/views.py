414e3c6fbdc499b312bfbb3ab400a9344c576b03
Muhindo Kiro <blaize1143@gmail.com>
2022-05-10 13:15:58 +0300

    import response and status from restframework,,add post method to add a new object

--- KiroTest/views.py
+++ KiroTest/views.py
@@ -3,13 +3,22 @@ from .models import Drink
 from django.http import JsonResponse
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 @api_view('GET', 'POST')
 def cocktail_list(request): 
 
-    cocktails =  Drink.objects.all()
-
-    serializer = DrinkSerializer(cocktails, many=True)
-    return JsonResponse({'cocktails': serializer.data})
+    if request.method == 'GET':
+        cocktails =  Drink.objects.all()
+        serializer = DrinkSerializer(cocktails, many=True)
+        return JsonResponse({'cocktails': serializer.data})
+    
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
